bb3d569dea82d0984c5fc3b283545c8b27061188
aloondraw <alondraguga@gmail.com>
2019-03-28 14:43:02 -0400

    update

---
+++ GMapsTest/src/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { CognitoServiceProvider } from "../../providers/cognito-service/cognito-service";
+
+import { HomePage } from '../home/home';
+
+let flag: boolean = false;
+
+@Component({
+  selector: "page-login",
+  templateUrl: "login.html"
+})
+export class LoginPage {
+  email: string;
+  password: string;
+
+  constructor(public CognitoSerive:CognitoServiceProvider, public navCtrl: NavController) {
+  }
+
+  login(){
+    this.CognitoSerive.authenticate(this.email, this.password)
+    .then(res =>{
+      flag = true;
+      console.log(res);
+    }, err =>{
+      flag = false;
+      console.log(err);
+    });
+  }
+  goTo(page) {
+    if(page === 'home' && flag) {
+      this.navCtrl.push(HomePage);
+    }
+
+  }
+}
